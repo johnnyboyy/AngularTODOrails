09a7dc0367e53ab33b77a50561afd26ad7a37eb8
John Zdanis <Zdanis.john@gmail.com>
2013-11-29 22:33:01 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularTODOrails::Application.config.secret_key_base = '91a77fb1bb4a82562265c26b44a18ef407a746a63376bf8cfd3f1656e531b51da1d2e3337b27ddbb4a4f301a47da694423df76d42a988694ebf15063de6be7fa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AngularTODOrails::Application.config.session_store :cookie_store, key: '_AngularTODOrails_session'
